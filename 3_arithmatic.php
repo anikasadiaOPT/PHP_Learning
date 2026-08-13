cc3dd5c66c08d62503bc91f00d3e9f914f3f0557
anikasadiaOPT <anikasadia2723@gmail.com>
2026-08-13 22:17:17 +0600

    Added arithmati learning file.

---
+++ 3_arithmatic.php
@@ -0,0 +1,28 @@
+<?php 
+    // arithmatic 
+    // +, -, *, / 
+
+    //Increment , dec
+    // ++ , --
+
+    // operator precedence
+    // ()
+    //**
+    // * / %
+    // + - 
+    $x = 1;
+    $y = 2;
+    $z = null;
+    $z = $x + $y;
+    $z = $x - $y;
+    $z = $x * $y;
+    $z = $x / $y;
+    $z = $x % $y;
+
+    $counter = 0;
+    $counter++;
+    $counter--;
+    $counter += 2;
+    $counter -= 1;
+    
+?>
