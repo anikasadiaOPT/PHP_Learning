30a6eaf4f36485afead9f0f2c39774c5a82d80a2
anikasadiaOPT <anikasadia2723@gmail.com>
2026-08-13 22:15:26 +0600

    Added first PHP Learning file.

---
+++ 1_index.php
@@ -0,0 +1,20 @@
+<?php 
+    echo "I like pizza <br>";
+    echo "I like pasta";
+    //This is a comment 
+
+    /* This is a multi-line comment
+       that spans multiple lines */
+
+?>
+
+<DOCTYPE html>
+<html>
+<head>
+    <title>My Favorite Foods</title>
+</head>
+<body>
+    <br>
+    <button> order pizza</button>
+</body>
+</html>
